51600098a7e4a9473c5c9aa2d287f6221ac9696c
pamela_afful <pamelafful56014@gmail.com>
2026-04-26 23:25:06 +0200

    modified the splitter seed

--- boston_data_splitter.R
+++ boston_data_splitter.R
@@ -3,7 +3,7 @@ rm(list=ls())
 #setwd("directory where data file is saved")
 fulldata <- read.csv("boston.csv") #Read in all 506 observations
 
-set.seed(1) #Insert your own project number here
+set.seed(718) #Insert your own project number here
 #Example: If your project number is 10, run the line set.seed(10)
 
 my_data <- fulldata[sample(1:nrow(fulldata), 400, replace=FALSE), ]
